add4814cf889b6025c546daeeea8c315c247c880
Edgar Glowacki <edgar_glowacki@yahoo.com>
2020-07-06 16:26:04 -0700

    Compiling fixes.

--- development/Common/Utility/source/Metrics/Concurrency.cpp
+++ development/Common/Utility/source/Metrics/Concurrency.cpp
@@ -4,16 +4,19 @@
 #include "Platform/Support.h"
 #include "YagetVersion.h"
 #include "Debugging/DevConfiguration.h"
-#include "rad_tm.h"
-
-#include <cvmarkersobj.h>
-namespace conc = Concurrency::diagnostic;
 
 #if YAGET_CONC_METRICS_ENABLED == 1
     #pragma message("======== Concurenty Metrics Enabled ========")
 #else
-    #error  "YAGET_METRICS_USE_CONC is not define, there is no support for turning off metrics at this time"
+    #pragma message("======== Concurenty Metrics NOT Included ========")
+//#error  "YAGET_METRICS_USE_CONC is not define, there is no support for turning off metrics at this time"
 #endif // YAGET_CONC_METRICS_ENABLED
+
+#if YAGET_CONC_METRICS_ENABLED == 1
+#include "rad_tm.h"
+
+#include <cvmarkersobj.h>
+namespace conc = Concurrency::diagnostic;
 
 namespace
 {
@@ -248,3 +251,6 @@ void yaget::metrics::MarkEndTimeSpan(uint64_t spanId, const char* file, uint32_t
 {
     tmEndTimeSpanEx(CaptureMask, spanId, file, line);
 }
+#else
+
+#endif // YAGET_CONC_METRICS_ENABLED
