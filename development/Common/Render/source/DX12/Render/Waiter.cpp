ff259b8a7bc04534968ad6ed4f157032a56c84c6
Edgar Glowacki <edgar_glowacki@yahoo.com>
2021-07-11 12:08:30 -0700

    Added thread name to log for waiter

--- development/Common/Render/source/DX12/Render/Waiter.cpp
+++ development/Common/Render/source/DX12/Render/Waiter.cpp
@@ -1,15 +1,17 @@
 #include "Render/Waiter.h"
+#include "Platform/Support.h"
+
 
 //-------------------------------------------------------------------------------------------------
 void yaget::render::Waiter::Wait()
 {
     if (mPauseCounter == true)
     {
-        YLOG_NOTICE("DEVI", "Waiter - We are requested to pause. Stopping.");
+        YLOG_NOTICE("DEVI", "Waiter - We are requested to pause from Thread: '%s'. Stopping.", yaget::platform::GetCurrentThreadName().c_str());
         mWaitForRenderThread.notify_one();
         std::unique_lock<std::mutex> locker(mPauseRenderMutex);
         mRenderPaused.wait(locker);
-        YLOG_NOTICE("DEVI", "Waiter - Resuming Render.");
+        YLOG_NOTICE("DEVI", "Waiter - Resuming Render from Thread: '%s'.", yaget::platform::GetCurrentThreadName().c_str());
     }
 }
 
@@ -25,11 +27,11 @@ void yaget::render::Waiter::BeginPause()
     }
 
     // We should use Concurrency (perf) locker to keep track in RAD
-    YLOG_NOTICE("DEVI", "Waiter - Requesting Render pause...");
+    YLOG_NOTICE("DEVI", "Waiter - Requesting Render pause from Thread: '%s'.", yaget::platform::GetCurrentThreadName().c_str());
     std::unique_lock<std::mutex> locker(mPauseRenderMutex);
     mPauseCounter = true;
     mWaitForRenderThread.wait(locker);
-    YLOG_NOTICE("DEVI", "Waiter - Render is Paused (resizing commences...)");
+    YLOG_NOTICE("DEVI", "Waiter - Render is Paused (resizing commences...) from Thread: '%s'.", yaget::platform::GetCurrentThreadName().c_str());
 }
 
 
@@ -41,7 +43,7 @@ void yaget::render::Waiter::EndPause()
         return;
     }
 
-    YLOG_NOTICE("DEVI", "Waiter - Render can start (resizing done)");
+    YLOG_NOTICE("DEVI", "Waiter - Render can start (resizing done) from Thread: '%s'.", yaget::platform::GetCurrentThreadName().c_str());
     mPauseCounter = false;
     mRenderPaused.notify_one();
 }
