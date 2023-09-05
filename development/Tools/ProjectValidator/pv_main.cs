2785e0d489c83f3e93a7488d885a66ce3ef5754f
Edgar Glowacki <edgar_glowacki@yahoo.com>
2023-09-04 22:25:34 -0700

    Added C# pv project

---
+++ development/Tools/ProjectValidator/pv_main.cs
@@ -0,0 +1,11 @@
+﻿namespace pv
+{
+    internal class pv_main
+    {
+        static void Main(string[] args)
+        {
+            var arguments = string.Join( ",", args.ToArray());
+            Console.WriteLine("Hello, World!, '" + arguments + "'.");
+        }
+    }
+}
