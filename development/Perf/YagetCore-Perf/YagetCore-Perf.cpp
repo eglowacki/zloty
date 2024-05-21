3f7c40c97751417f88799de8b63083c4dec7edcc
Edgar <edgar_glowacki@yahoo.com>
2024-05-21 23:02:25 +0200

    Added yaget init code to main

--- development/Perf/YagetCore-Perf/YagetCore-Perf.cpp
+++ development/Perf/YagetCore-Perf/YagetCore-Perf.cpp
@@ -1,20 +1,31 @@
 // YagetCore-Perf.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "YagetCore.h"
 #include <iostream>
+
+
+namespace yaget::ylog
+{
+  yaget::Strings GetRegisteredTags()
+  {
+      yaget::Strings tags =
+      {
+          #include "Logger/CoreLogTags.h"
+      };
+
+      return tags;
+  }
+} // namespace yaget::ylog
+
+
+YAGET_BRAND_NAME_F("Beyond Limits")
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace yaget;
+
+    /*auto result =*/ system::InitializeSetup();
+    //std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
