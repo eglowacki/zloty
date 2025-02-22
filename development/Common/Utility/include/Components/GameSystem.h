694dd88444db227bb5b7997e896e14795f4136c7
Edgar Glowacki <edgar_glowacki@yahoo.com>
2025-02-22 14:40:53 -0800

    Removed yaget namespace from variable declerations

--- development/Common/Utility/include/Components/GameSystem.h
+++ development/Common/Utility/include/Components/GameSystem.h
@@ -58,7 +58,7 @@ namespace yaget::comp::gs
         using Row = typename RowPolicy::Row;
         using CoordinatorSet = CS;
 
-        using UpdateFunctor = std::function<void(yaget::comp::Id_t id, const time::GameClock& gameClock, metrics::Channel& channel, Comps&... args)>;
+        using UpdateFunctor = std::function<void(comp::Id_t id, const time::GameClock& gameClock, metrics::Channel& channel, Comps&... args)>;
 
         // framework calls this on same cadence (every tick...)
         // In default case that is SystemsCoordinator class
@@ -76,7 +76,7 @@ namespace yaget::comp::gs
         Messaging& mMessaging;
 
     private:
-        void Update(yaget::comp::Id_t id, const time::GameClock& gameClock, metrics::Channel& channel, const Row& row);
+        void Update(comp::Id_t id, const time::GameClock& gameClock, metrics::Channel& channel, const Row& row);
 
         const char* mNiceName = nullptr;
         UpdateFunctor mUpdateFunctor;
