bca19198a4c207e72075d5dcd6b4665ea6faec0e
Edgar Glowacki <edgar_glowacki@yahoo.com>
2025-06-30 21:52:11 -0700

    Added dash line comment between methods

--- development/Games/Defensor/source/Squadron/MenuSystem.cpp
+++ development/Games/Defensor/source/Squadron/MenuSystem.cpp
@@ -1,11 +1,14 @@
 ﻿#include "MenuSystem.h"
 
 
+//-------------------------------------------------------------------------------------------------
 defensor::game::MenuSystem::MenuSystem(Messaging& messaging, Application& app, GameCoordinatorSet& coordinatorSet)
     : GameSystem("MenuSystem", messaging, app, [this](auto&&... params) {OnUpdate(params...); }, coordinatorSet)
 {
 }
 
+
+//-------------------------------------------------------------------------------------------------
 void defensor::game::MenuSystem::OnUpdate(yaget::comp::Id_t id, const yaget::time::GameClock& gameClock, yaget::metrics::Channel& channel, yaget::comp::MenuComponent* menuComponent, comp::InputComponent* /*inputComponent*/, comp::ScriptComponent* scriptComponent)
 {
     id; gameClock; channel;
