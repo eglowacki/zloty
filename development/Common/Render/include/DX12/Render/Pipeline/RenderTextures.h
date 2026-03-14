735b15161298a28c05648567b5b3f02467cc2b06
Edgar Glowacki <edgar_glowacki@yahoo.com>
2026-03-14 17:23:10 +0100

    Added RenderTextures class to cache images

---
+++ development/Common/Render/include/DX12/Render/Pipeline/RenderTextures.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////
+// RenderTextures.h
+//
+//  Copyright 03/14/2026 Edgar Glowacki.
+//
+//  Maintained by: Edgar
+//
+//  NOTES:
+//
+//  #include "Render/Pipeline/RenderTextures.h"
+//
+//////////////////////////////////////////////////////////////////////
+//! \file
+#pragma once
+#include "Render/RenderCore.h"
+#include "Streams/Buffers.h"
+#include "VTS/VirtualTransportSystem.h"
+#include "Render/Cache/CacheWatcher.h"
+
+
+namespace yaget::render
+{
+
+    //-------------------------------------------------------------------------------------------------
+    class RenderTextures : public CacheWatcher<io::Buffer>
+    {
+    public:
+        RenderTextures(io::VirtualTransportSystem& vts);
+        ~RenderTextures();
+
+
+        io::Buffer GetTexture(const io::Tag& tag);
+        std::vector<io::Buffer> GetTextures(const io::Tags& tags);
+
+        static void PopulateTextureMappings(io::VirtualTransportSystem::Section fileName, io::VirtualTransportSystem& vts);
+        static void SaveTextureMappings(io::VirtualTransportSystem::Section fileName, io::VirtualTransportSystem& vts);
+
+    private:
+        io::Buffer AssureTextureNonMT(const yaget::io::Tag& tag);
+
+    };
+
+}
