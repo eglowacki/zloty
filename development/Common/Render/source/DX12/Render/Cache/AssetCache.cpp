c189ac456b620013d10fe3cec4df356b11644a13
Edgar <edgar_glowacki@yahoo.com>
2026-08-26 17:58:17 -0700

    Whan asset in cache is newer, mark it as Holes

--- development/Common/Render/source/DX12/Render/Cache/AssetCache.cpp
+++ development/Common/Render/source/DX12/Render/Cache/AssetCache.cpp
@@ -90,6 +90,7 @@ yaget::render::AssetCache::AssetCache(io::VirtualTransportSystem& vts, Section f
                     {
                         // skip getting cache data for this file since it's newer then cache file
                         YLOG_INFO("DEVI", std::format("File '{} ({})' is newer then cache '{} ({})', will update.", filePath, fileTimeStamp, cacheFilePath, cacheFileTimeStamp).c_str());
+                        mCacheStatus = ored(mCacheStatus, CacheStatus::Holes);
                         continue;
                     }
 
